e0aaaaae0ab46e67f837161ca69b93e7bd502298
song <rosetinsong@outlook.com>
2024-10-20 23:00:27 +0800

    调用API成功

--- src/frcobot_hw/src/frcobot_cmd_demo.cpp
+++ src/frcobot_hw/src/frcobot_cmd_demo.cpp
@@ -22,6 +22,17 @@ int main(int argc, char** argv)
     JointPos j_rad, j_pose_inver;
     ExaxisPos epos;
 
+    // Target test
+    JointPos j1; DescPose desc_pos1;
+    memset(&j1, 0, sizeof(JointPos));
+    memset(&desc_pos1, 0, sizeof(DescPose));
+    j1 = {76.764,-89.996735,-90.000221,-180.001526,-90.004128,90.000000};
+    robot.GetForwardKin(&j1, &desc_pos1);
+    printf("Target joint pos:%f,%f,%f,%f,%f,%f\n", j1.jPos[0],j1.jPos[1],j1.jPos[2],j1.jPos[3],j1.jPos[4],j1.jPos[5]);
+    printf("Target desc_pose tcp pose : %f, %f, %f, %f, %f, %f\n", desc_pos1.tran.x, desc_pos1.tran.y,  desc_pos1.tran.z, desc_pos1.rpy.rx, desc_pos1.rpy.ry, desc_pos1.rpy.rz);
+    printf("-----------------\n");
+
+
     int tool = 0;
     int user = 0;
     float vel = 100.0;
@@ -55,6 +66,10 @@ int main(int argc, char** argv)
 
     int  ret = robot.GetInverseKin(type, &flange, -1, &j_pose_inver);
     printf("joint pos InverseKin:%d,%f,%f,%f,%f,%f,%f\n", ret, j_pose_inver.jPos[0],j_pose_inver.jPos[1],j_pose_inver.jPos[2],j_pose_inver.jPos[3],j_pose_inver.jPos[4],j_pose_inver.jPos[5]);
+    
+    // 设置目标值
+    // j_pose_inver.jPos[0] += 20;
+    // printf("Target joint pos InverseKin:%d,%f,%f,%f,%f,%f,%f\n", ret, j_pose_inver.jPos[0],j_pose_inver.jPos[1],j_pose_inver.jPos[2],j_pose_inver.jPos[3],j_pose_inver.jPos[4],j_pose_inver.jPos[5]);
 
     robot.GetForwardKin(&j_pose_inver, &desc_pose);
     printf("desc_pose tcp pose : %f, %f, %f, %f, %f, %f\n", desc_pose.tran.x, desc_pose.tran.y,  desc_pose.tran.z, desc_pose.rpy.rx, desc_pose.rpy.ry, desc_pose.rpy.rz);
@@ -65,11 +80,11 @@ int main(int argc, char** argv)
     robot.ComputePostPick(&desc_pose, -100, 0, &post_pose);
     printf("post_pose tcp pose : %f, %f, %f, %f, %f, %f\n", post_pose.tran.x, post_pose.tran.y,  post_pose.tran.z, post_pose.rpy.rx, post_pose.rpy.ry, post_pose.rpy.rz);
 
-    int err1 = robot.MoveJ(&j_pose_inver, &flange, tool, user, vel, acc, ovl, &epos, blendT,flag, &offset_pose);
+    int err1 = robot.MoveJ(&j1, &desc_pos1, tool, user, vel, acc, ovl, &epos, blendT,flag, &offset_pose);
     printf("movej code:%d\n", err1);
 
-    int err2 = robot.MoveL(&j_pose_inver, &flange, tool, user, vel, acc, ovl, blendR, &epos,search,flag, &offset_pose);
-    printf("movel code:%d\n", err2);
+    // int err2 = robot.MoveL(&j_pose_inver, &flange, tool, user, vel, acc, ovl, blendR, &epos,search,flag, &offset_pose);
+    // printf("movel code:%d\n", err2);
 
     robot.SetToolDO(0, status_1, smooth, block);
     robot.SetToolDO(1, status_0, smooth, block);
